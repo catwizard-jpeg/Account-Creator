1ed6ce9f032635958e00cc040f29b8c6cc2ae01e
Leonardo Salvarani <S2402984@ndonline.ac.uk>
2024-10-07 15:33:40 +0100

    added 5 more tests

--- Account Creator/Program_Testing.py
+++ Account Creator/Program_Testing.py
@@ -10,11 +10,20 @@ class TestGenerateUsername(unittest.TestCase):
     def test_username_02(self):
         username = generateUsername("John Doe", "2023")
         self.assertEqual(username, "23DoeJ", "username incorrect")
+    def test_username_03(self):
+        username = generateUsername("Jane Ane", "2019")
+        self.assertEqual(username, "19AneJ", "username incorrect")
         
 class TestCalculateYearGroup(unittest.TestCase):
     def test_year_group_01(self):
         yearGroup = calculateYearGroup("22smithR")
         self.assertEqual(yearGroup,"Year 9", "year group incorrect")
+    def test_year_group_02(self):
+        yearGroup = calculateYearGroup("23DoeJ")
+        self.assertEqual(yearGroup,"Year 8", "year group incorrect")
+    def test_year_group_03(self):
+        yearGroup = calculateYearGroup("19AneJ")
+        self.assertEqual(yearGroup,"Year 12", "year group incorrect")
 
 class TestIsPasswordSecure(unittest.TestCase):
     def test_is_password_secure_01(self):
@@ -22,6 +31,16 @@ class TestIsPasswordSecure(unittest.TestCase):
         username = "22SmithR"
         isSecure = isPasswordSecure(password, username)
         self.assertEqual(isSecure,False, "password security incorrect")
+    def test_is_password_secure_02(self):
+        password = "P@ssw0rd"
+        username = "22SmithR"
+        isSecure = isPasswordSecure(password, username)
+        self.assertEqual(isSecure,True, "password security incorrect")
+    def test_is_password_secure_03(self):
+        password = "P@ssw0rd"
+        username = "23DoeJ"
+        isSecure = isPasswordSecure(password, username)
+        self.assertEqual(isSecure,True, "password security incorrect")
 
 if __name__ == "__main__":
     unittest.main()
